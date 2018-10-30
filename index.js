b6d91659c00201942d177d53f293f5929c0db241
Amaan Ahmed <amaan.ahmed3@gmail.com>
2018-10-30 14:44:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// add solution here
+function theBeatlesPlay (arrayM, arrayI) {
+  var i = [];
+  for (let j = 0; j < 4; j++) {
+    i.push(`${arrayM[j]} plays ${arrayI[j]}`);
+  }
+return i;
+}
+function johnLennonFacts (array) {
+  let j = 0;
+  while (j < array.length) {
+  array[j] = array [j++] + "!!!"
+  }
+  return array;
+}
